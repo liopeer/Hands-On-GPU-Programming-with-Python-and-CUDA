cb33d8fa704df0ab14d5adc837b533ba3d7437d4
lionel <lionel.peer@gmail.com>
2021-11-03 18:11:06 +0100

    blub

--- Chapter03/mydeviceQuery.py
+++ Chapter03/mydeviceQuery.py
@@ -1,4 +1,8 @@
 import pycuda.driver as drv
 drv.init()
 
-print('Detected {} CUDA capable device(s)'.format(drv.Device.count()))
+for i in range(drv.Device.count()):
+    gpu_device = drv.Device(i)
+    print('Device {}: {}'.format(i, gpu_device.name()))
+    compute_capability = float('%d.%d' %gpu_device.compute_capability())
+    print('\t Total Memory: {} megabytes'.format(gpu_device.total_memory()//(1024**2)))
