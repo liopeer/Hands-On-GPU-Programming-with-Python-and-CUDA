bfe13479eb357ce962f0ab2852a85cffdfb3d0ab
lionel <lionel.peer@gmail.com>
2021-11-04 13:17:48 +0100

    blubi

--- Chapter03/mydeviceQuery.py
+++ Chapter03/mydeviceQuery.py
@@ -6,3 +6,8 @@ for i in range(drv.Device.count()):
     print('Device {}: {}'.format(i, gpu_device.name()))
     compute_capability = float('%d.%d' %gpu_device.compute_capability())
     print('\t Total Memory: {} megabytes'.format(gpu_device.total_memory()//(1024**2)))
+    device_attributes_tuples = gpu_device.get_attributes().iteritems()
+    device_attributes = {}
+
+    for k, v in device_attributes_tuples:
+        device_attributes[str(k)] = v
