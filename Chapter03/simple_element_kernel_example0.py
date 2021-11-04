0579d615ceec9e0236bd23e6801afbfaa85f0fe5
lionel <lionel.peer@gmail.com>
2021-11-04 16:11:04 +0100

    update to python3

--- Chapter03/simple_element_kernel_example0.py
+++ Chapter03/simple_element_kernel_example0.py
@@ -4,7 +4,7 @@ from pycuda import gpuarray
 from time import time
 from pycuda.elementwise import ElementwiseKernel
 
-host_data = np.float32( np.random.random(50000000) )
+host_data = np.float32( np.random.random(50000000) ) # 50million
 
 gpu_2x_ker = ElementwiseKernel(
 "float *in, float *out",
@@ -15,7 +15,8 @@ def speedcomparison():
     t1 = time()
     host_data_2x =  host_data * np.float32(2)
     t2 = time()
-    print 'total time to compute on CPU: %f' % (t2 - t1)
+    print('total time to compute on CPU: %f' % (t2 - t1))
+
     device_data = gpuarray.to_gpu(host_data)
     # allocate memory for output
     device_data_2x = gpuarray.empty_like(device_data)
@@ -23,8 +24,8 @@ def speedcomparison():
     gpu_2x_ker(device_data, device_data_2x)
     t2 = time()
     from_device = device_data_2x.get()
-    print 'total time to compute on GPU: %f' % (t2 - t1)
-    print 'Is the host computation the same as the GPU computation? : {}'.format(np.allclose(from_device, host_data_2x) )
+    print('total time to compute on GPU: %f' % (t2 - t1))
+    print('Is the host computation the same as the GPU computation? : {}'.format(np.allclose(from_device, host_data_2x) ))
     
 
 if __name__ == '__main__':
--- Chapter03/time_calc0.py
+++ Chapter03/time_calc0.py
@@ -4,13 +4,13 @@ from pycuda import gpuarray
 from time import time
 
 
-host_data = np.float32( np.random.random(50000000) )
+host_data = np.float32( np.random.random(200000000) ) #200 million
 
 t1 = time()
 host_data_2x =  host_data * np.float32(2)
 t2 = time()
 
-print 'total time to compute on CPU: %f' % (t2 - t1)
+print('total time to compute on CPU: %f' % (t2 - t1))
 
 
 device_data = gpuarray.to_gpu(host_data)
@@ -22,5 +22,5 @@ t2 = time()
 from_device = device_data_2x.get()
 
 
-print 'total time to compute on GPU: %f' % (t2 - t1)
-print 'Is the host computation the same as the GPU computation? : {}'.format(np.allclose(from_device, host_data_2x) )
+print('total time to compute on GPU: %f' % (t2 - t1))
+print('Is the host computation the same as the GPU computation? : {}'.format(np.allclose(from_device, host_data_2x) ))
